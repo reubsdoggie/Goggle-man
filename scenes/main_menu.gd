db42a953c6bba42396e01ef140fa5e638cabdbd6
reubsdoggie <reubsaw@icloud.com>
2024-02-03 11:18:22 +0000

    1.1 files have been added!

---
+++ scenes/main_menu.gd
@@ -0,0 +1,9 @@
+extends Node
+
+
+func _on_level_1_pressed():
+	get_tree().change_scene_to_file("res://scenes/level1.tscn")
+
+
+func _on_level_2_pressed():
+	get_tree().change_scene_to_file("res://scenes/level2.tscn")

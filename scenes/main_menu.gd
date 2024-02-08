4dc8827b7fb741f290a6349c239f6e8267bedd77
reubsdoggie <reubsaw@icloud.com>
2024-02-08 18:37:34 +0000

    1.3

--- scenes/main_menu.gd
+++ scenes/main_menu.gd
@@ -12,3 +12,7 @@ func _on_level_2_pressed():
 
 func _on_level_3_pressed():
 	get_tree().change_scene_to_file("res://scenes/level3.tscn")
+
+
+func _on_level_4_pressed():
+	get_tree().change_scene_to_file("res://scenes/level4.tscn")
